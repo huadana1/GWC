f706b749c67465cc9cbf5fb298d506785f79c8e1
huadana1 <41088940+huadana1@users.noreply.github.com>
2018-07-10 15:25:01 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,23 @@
+//Javascripts for my HTML Webpage
+
+//enlarges a picture when mouse hover
+function enlargePic(pic) {
+  pic.style.height = "100%"
+  pic.style.width = "100%"
+}
+
+//returns picture to original size
+function normalPic(pic) {
+  pic.style.width = "75%";
+  pix.style.height = "75%";
+}
+
+//opens IG Page
+function openIG() {
+  window.open("https://www.instagram.com/huadana1/")
+}
+
+//opnes FB Page
+function openFB() {
+  window.open("https://www.facebook.com/dana.hua.90")
+}
